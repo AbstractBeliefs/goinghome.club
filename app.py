8510eb1f0a0e671423c6a6a320592d9b580dc54f
Kaffse <kaffse@gmail.com>
2015-04-30 20:23:22 +0100

    Init flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+@app.route('/')
+def index():
+    return render_template("index.jade")
+
+@app.route('/dragndrop')
+def dragndrop():
+    return render_template("dragndrop.jade")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
